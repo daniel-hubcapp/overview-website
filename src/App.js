ae88fb5a96efcb3561cd234b78d3697af9fdf12d
Daniel Alter <daniel.united@windowslive.com>
2021-05-08 00:09:14 +0300

    ➕Installed React + Tailwind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-black text-white h-screen flex flex-col items-center justify-center">
+      <div className="flex-row">
+        <h1>Overview</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;
