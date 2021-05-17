86e65e40ddb6fe671210438471495071561c2b52
Daniel Alter <daniel.united@windowslive.com>
2021-05-17 10:31:53 +0300

    Converted video to webm

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "../src/fonts/stylesheet.css";
 import ReactPlayer from "react-player/file";
-import video from "../src/hero.mp4";
+import video from "../src/sunrise.webm";
 import static_background from "../src/static-background.jpeg";
 // import React, { useState } from "react";
 
 function App() {
   return (
     <div className="bg-black text-white h-screen flex flex-col items-center justify-end py-10 px-6">
-      <div className="text-center max-w-3xl relative z-10">
-        <h1 className="text-5xl font-bold mb-4 sm:text-6xl">Can You See It?</h1>
-        <p className="sm:text-lg">
+      <div className="text-center max-w-2xl relative z-10">
+        <h1 className="text-6xl font-bold mb-5 sm:text-6xl">Can You See It?</h1>
+        <p>
           The Overview Effect is a cognitive shift in awareness reported by
-          astronauts during spaceflight, often while viewing the Earth from
+          astronauts while viewing the Earth from
           orbit or from the lunar surface. It refers to the experience of seeing
           firsthand the reality of the Earth in space, which is immediately
           understood to be a tiny, fragile ball of life, hanging in the void,
@@ -19,7 +19,7 @@ function App() {
         </p>
       </div>
       <ReactPlayer
-        className="fixed inset-0 z-0 h-full object-cover sm:bg-none bg-hero-background-image bg-center bg-no-repeat"
+        className="fixed inset-0 z-0 h-full object-cover sm:bg-none bg-center bg-no-repeat"
         url={video}
         width="100%"
         height="100%"
